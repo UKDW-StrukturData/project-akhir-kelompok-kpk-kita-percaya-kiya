563bc4830439527fb1e95e671d615484e69dd901
leutenantKiya <antoniuskiya57@gmail.com>
2025-11-13 11:37:33 +0700

    finish searching by matching characters (Fransiska)

--- gemini.py
+++ gemini.py
@@ -1,5 +1,10 @@
-import streamlit as st
 from google import genai
 
-def geminiSearch(prompt):
-    st.info("Sabar ya le masih dikembangin 🥹")
+api_key =  "AIzaSyC-IQvTBHUA3Y2A5IlQTmsHw-zQ3o_XZvc"
+client = genai.Client(api_key= api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
